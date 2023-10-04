23731c22f89c49e7ff94032791eb0a9abaf9e30e
frdrk00 <masterkooo@gmail.com>
2023-10-04 17:44:17 +0200

    Setup

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,86 +1,74 @@
-import { useLogin } from "@refinedev/core";
-import { useEffect, useRef } from "react";
+import { useLogin } from '@refinedev/core'
+import { useEffect, useRef } from 'react'
 
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import { ThemedTitleV2 } from "@refinedev/mui";
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
 
-import { CredentialResponse } from "../interfaces/google";
+import { yariga } from '../assets'
 
-// Todo: Update your Google Client ID here
-const GOOGLE_CLIENT_ID =
-  "1041339102270-e1fpe2b6v6u1didfndh7jkjmpcashs4f.apps.googleusercontent.com";
+import { CredentialResponse } from '../interfaces/google'
 
 export const Login: React.FC = () => {
-  const { mutate: login } = useLogin<CredentialResponse>();
+  const { mutate: login } = useLogin<CredentialResponse>()
 
   const GoogleButton = (): JSX.Element => {
-    const divRef = useRef<HTMLDivElement>(null);
+    const divRef = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
-      if (typeof window === "undefined" || !window.google || !divRef.current) {
-        return;
+      if (typeof window === 'undefined' || !window.google || !divRef.current) {
+        return
       }
 
       try {
         window.google.accounts.id.initialize({
-          ux_mode: "popup",
-          client_id: GOOGLE_CLIENT_ID,
+          ux_mode: 'popup',
+          client_id: process.env.REACT_APP_GOOGLE_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
-              login(res);
+              login(res)
             }
           },
-        });
+        })
         window.google.accounts.id.renderButton(divRef.current, {
-          theme: "filled_blue",
-          size: "medium",
-          type: "standard",
-        });
+          theme: 'filled_blue',
+          size: 'medium',
+          type: 'standard',
+        })
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
-    }, []);
+    }, [])
 
-    return <div ref={divRef} />;
-  };
+    return <div ref={divRef} />
+  }
 
   return (
-    <Container
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+    <Box
+      component="div"
+      sx={{
+        backgroundColor: '#FCFCFC',
       }}
     >
-      <Box
-        display="flex"
-        gap="36px"
-        justifyContent="center"
-        flexDirection="column"
+      <Container
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
       >
-        <ThemedTitleV2
-          collapsed={false}
-          wrapperStyles={{
-            fontSize: "22px",
-            justifyContent: "center",
-          }}
-        />
-
-        <GoogleButton />
-
-        <Typography align="center" color={"text.secondary"} fontSize="12px">
-          Powered by
-          <img
-            style={{ padding: "0 5px" }}
-            alt="Google"
-            src="https://refine.ams3.cdn.digitaloceanspaces.com/superplate-auth-icons%2Fgoogle.svg"
-          />
-          Google
-        </Typography>
-      </Box>
-    </Container>
-  );
-};
+        <Box
+          display="flex"
+          gap="36px"
+          justifyContent="center"
+          flexDirection="column"
+        >
+          <img alt="Logo" src={yariga} />
+          <Box mt={4}>
+            <GoogleButton />
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  )
+}
